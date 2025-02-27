1226c9a84991e902dc3f9fe397e74c0689e9178f
罗浩宇 <23373112@buaa.edu.cn>
2025-02-27 20:32:25 +0800

    rename main

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,7 +3,7 @@ import expr.Expr;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Main {
+public class MainClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
